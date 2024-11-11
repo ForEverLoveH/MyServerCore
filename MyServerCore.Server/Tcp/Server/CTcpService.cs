690f67a1944ddc630a5f073ce347cc7640ba2ce2
luohaiqiang <106363711+ForEverLoveH@users.noreply.github.com>
2024-11-11 14:44:07 +0800

    修改发送与接收

--- MyServerCore.Server/Tcp/Server/CTcpService.cs
+++ MyServerCore.Server/Tcp/Server/CTcpService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using MyServerCode.Summer.Service;
 using MyServerCode.Summer.Service.Tcp;
+using Newtonsoft.Json;
 
 namespace MyServerCore.Server.Tcp.Server;
 
@@ -37,9 +39,40 @@ public class CTcpService:TcpService
     {
         return new CTcpSession(this);
     }
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="error"></param>
     protected override void OnError(SocketError error)
     {
         
+    }
+    public  void cSendData<T>(T data) where T : class
+    {
+        if(data == null)return  ;
+        string json = JsonConvert.SerializeObject(data);
+         SendData(json);     
+    }
+    public void SendData(string json)
+    {
+        if (!string.IsNullOrEmpty(json))
+        {
+            byte[] message = Encoding.UTF8.GetBytes(json);
+            SendData(message);
+        }
+    }
+    public void SendData(byte[] message)
+    {
+        int length = message.Length;
+        byte[] buffer = BitConverter.GetBytes(length);
+        if (buffer.Length > 0)
+        {
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(buffer);
+            }
+            byte[] result = buffer.Concat(message).ToArray();
+            SendData(result);
+        }
     }
 }
