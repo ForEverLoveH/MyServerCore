a1d824fe96fc87d58590fb0f17d9681f0acc811d
luohaiqiang <106363711+ForEverLoveH@users.noreply.github.com>
2024-11-22 10:06:03 +0800

    日志木块

---
+++ MyServerCore.Log/Log/MyLogColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyServerCore.Log.Log
+{
+    public enum YNetLogColor
+    {
+        Red, Green, Blue, Cyan, Magentna, Yellow, None,
+    }
+}
